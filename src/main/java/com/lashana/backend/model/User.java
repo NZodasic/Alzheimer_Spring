4ef8079949b288029e32b0e555140d8cfbc26776
Nguyễn Võ Chí Dũng <bbiacm007@gmail.com>
2025-01-24 22:40:19 +0700

    feat: Update authentication api

---
+++ src/main/java/com/lashana/backend/model/User.java
@@ -0,0 +1,22 @@
+package com.lashana.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+}
